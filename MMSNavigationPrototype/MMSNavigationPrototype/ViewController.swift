f2fd6c6b3bece6cd5ef64c34514b89f83791cd72
srijansrv <srijansrv@gmail.com>
2016-03-10 00:48:08 +0530

    Added new project

---
+++ MMSNavigationPrototype/MMSNavigationPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MMSNavigationPrototype
+//
+//  Created by Srijan Srivastava on 10/03/16.
+//  Copyright © 2016 Morningstar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
